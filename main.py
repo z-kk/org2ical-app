a6afe83138dbae2c02c2b8bf856f6dc8b82e274a
z-kk <z-kk@outlook.jp>
2026-01-20 20:22:18 +0900

    use docopt

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
-import sys
-import org2ical
+"""org2ical
 
-def main():
+Usage:
+    org2ical <org_name> [<ics_name>]
+    org2ical -i <org_name> [-o <ics_name>]
+
+Options:
+    -i <org_name>   : target org file
+    -o <ics_name>   : output ics file
+"""
+import os
+import org2ical
+from docopt import docopt
+
+def main(org_name, ics_name):
     orgstr = ""
-    with open(sys.argv[1]) as f:
+    with open(org_name) as f:
         orgstr = f.read()
     ical, w = org2ical.loads(orgstr)
     assert w == []
-    with open(sys.argv[2], mode='w') as f:
+    with open(ics_name, mode='w') as f:
         f.write(ical)
 
 
+def get_org_name(args):
+    res = ""
+    if args["-i"]:
+        res = args["-i"]
+    elif args["<org_name>"]:
+        res = args["<org_name>"]
+    name, ext = os.path.splitext(res)
+    if ext != ".org":
+        res = name + ".org"
+    return res
+
+
+def get_ical_name(args):
+    res = ""
+    if args["-o"]:
+        res = args["-o"]
+    elif args["<ics_name>"]:
+        res = args["<org_name>"]
+    else:
+        res = get_org_name(args)
+    name, ext = os.path.splitext(res)
+    if ext != ".ics":
+        res = name + ".ics"
+    return res
+
+
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print(sys.argv[0] + " <org name> <ical name>")
-        sys.exit(1)
-    main()
+    args = docopt(__doc__)
+    org_name = get_org_name(args)
+    ics_name = get_ical_name(args)
+    main(org_name, ics_name)
